e4c13ebc0d3f9d3dc065cef6ba3290fb0b8e7233
Sergei-Illarionov <mestre_1000@mail.ru>
2021-03-13 20:10:56 +0300

    Added components Header, Navbar, Profile

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./Header.module.css"
+
+
+const Header = () => {
+    return (
+        <header className={s.item_header}>
+            <img
+                src="https://f0.pngfuel.com/png/1010/881/green-a-logo-png-clip-art.png" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://d9me64que7cqs.cloudfront.net/images/common/background-generic-landscape-short.31b43a7823e13c6383e5e613b04b5b58.jpg"
+                 alt="logo" />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
