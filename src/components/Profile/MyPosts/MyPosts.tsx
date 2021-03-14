89f4799ee6e638bf100c19014e05a940a0f168f9
Sergei-Illarionov <mestre_1000@mail.ru>
2021-03-14 22:25:22 +0300

    Added component MyPosts

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+const MyPosts = () => {
+    return (
+        <div >
+            <h3> My posts</h3>
+            <div>
+                New post
+            </div>
+            <div >
+                <div >
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyPosts;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,29 +1,24 @@
-import React from 'react';
+import React from "react";
+import MyPosts from "./MyPosts/MyPosts";
+
+
+
+
+
 
 const Profile = () => {
-    return <div className='content'>
-        <div>
-            <img src="https://d9me64que7cqs.cloudfront.net/images/common/background-generic-landscape-short.31b43a7823e13c6383e5e613b04b5b58.jpg"
-                 alt="logo" />
-        </div>
-        <div>
-            ava + description
-        </div>
-        <div>
-            My posts
+    return (
+        <div >
             <div>
-                New post
+                <img src="https://d9me64que7cqs.cloudfront.net/images/common/background-generic-landscape-short.31b43a7823e13c6383e5e613b04b5b58.jpg"
+                     alt="logo" />
             </div>
             <div>
-                <div>
-                    post 1
-                </div>
-                <div>
-                    post 2
-                </div>
+                ava + description
             </div>
-        </div>
+<MyPosts/>
     </div>
+    )
 }
 
 export default Profile;
